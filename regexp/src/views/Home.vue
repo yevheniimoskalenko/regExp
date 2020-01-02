346afef2298c30fc92668b6939eddcc3e2588cb3
Євгеній Москаленко <moskale2yevhenii@hneu.net>
2020-01-02 20:41:09 +0300

    install loader, font

---
+++ regexp/src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" class="logo" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: "home",
+  components: {}
+};
+</script>
+<style lang="sass" scoped>
+.logo {
+  margin-top: 20px;
+}
+</style>
